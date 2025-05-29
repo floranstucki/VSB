ad21a004ae5df3e8a8109637efdbf0096ae91dd2
floranstucki <34169819+floranstucki@users.noreply.github.com>
2025-05-29 14:12:00 +0200

    Delete includes directory

--- includes/ajouter-joueur.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-add_shortcode('ajouter', 'ajout_joueur_form');
-function ajout_joueur_form(){
-    include MY_PLUGIN_PATH . '/includes/templates/ajout-joueur-form.php';
-}
-
--- includes/modifier-joueur.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-add_shortcode('modifier', 'modifier_joueur_form');
-
-function modifier_joueur_form(){
-    include MY_PLUGIN_PATH . '/includes/templates/modifier-joueur-form.php';
-}
-
